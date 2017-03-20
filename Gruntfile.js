0e1efb5227fad6d29aa9c636d47b6a268d419d3e
Dizzyspb <derevoluza@mail.ru>
2017-03-20 16:17:22 +0300

    images

--- Gruntfile.js
+++ Gruntfile.js
@@ -420,13 +420,7 @@ module.exports = function (grunt) {
       ]
     },
 
-    // Test settings
-    //karma: {
-    // unit: {
-    //   configFile: 'test/karma.conf.js',
-    //   singleRun: true
-    //}
-    //}
+  
   });
 
 
